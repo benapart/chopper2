7e44e73da3b644ae404ec35dae3c3892bbc08da0
benapart <164779300+benapart@users.noreply.github.com>
2024-03-25 18:02:44 +0200

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,6 @@
 basic.forever(function () {
     music.play(music.createSoundExpression(WaveShape.Noise, 5000, 1, 255, 255, 5000, SoundExpressionEffect.None, InterpolationCurve.Linear), music.PlaybackMode.UntilDone)
     basic.showString("enemy found")
-    basic.showString("fifth level")
-    basic.showString("name chopper")
+    basic.showString("one hundred level")
+    basic.showString("namelaw ")
 })
